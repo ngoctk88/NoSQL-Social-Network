111221da05aabb581f338453a2ca75250de122a5
Victoria <vmcnorrill@gmail.com>
2023-05-24 19:03:53 -0400

    defined model to create docs

--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,39 @@
 const { Schema, model } = require('mongoose');
 
 // Schema to create User model
-const userSchema = new Schema(
-    {
-      username: {
+const userSchema = new Schema({
+    username: {
         type: String,
         required: true,
         unique: true,
         $trim: { input: ' ',  chars: ' ' },
-      },
-      email: {
+    },
+    email: {
         type: String,
         required: true,
         unique: true,
         match: /.+\@.+\..+/,
-      },
-      thoughts: ,
-      friends: ,
+    },
+    //   thoughts: ,
+    //   friends: ,
     }
-  );
+);
+  
+// model defined based on the schema to create documents
+const User = model('User', userSchema);
+
+// some seed data
+User
+    .create([
+        { username: 'bigbro', email: 'gmail@google.com' },
+        { username: 'officesweet', email: 'bill@microsoft.com' },
+        { username: 'SJisALIVE', email: 'test@apple.com' }
+    ])
+    .then(createdUsers => {
+        console.log(createdUsers);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+module.exports = User;
