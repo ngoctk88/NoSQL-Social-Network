dc9b9db82859fcaa99181670698fe9ae40f52e82
Victoria <vmcnorrill@gmail.com>
2023-05-25 20:39:09 -0400

    commented out my to do list for the thought controller

---
+++ controllers/thoughtControllers.js
@@ -0,0 +1,12 @@
+// /api/thoughts
+    // GET: all thoughts
+    // GET: a single thought by its _id
+    // POST: create a new thought
+        //(don't forget to push the created thought's _id 
+        //to the associated user's thoughts array field)
+    // PUT to update a thought by its _id
+    // DELETE to remove a thought by its _id
+
+// /api/thoughts/:thoughtId/reactions
+    // POST: create a reaction stored in a single thought's reactions array field
+    // DELETE: pull and remove a reaction by the reaction's reactionId value
